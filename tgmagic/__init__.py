32df01ee27ff6ff6a9f67a8a604c1a28cfb5361d
tezmen <tezmenpro@gmail.com>
2017-03-07 00:09:01 +0300

    Add init file

---
+++ tgmagic/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .bot import MagicFunction
+from .buttons import PrevButton, Buttons, ExtendNames
+from .helper import singleton, menu
+from .menu import MagicMenu
+
+__author__ = 'tezmen'
+__version__ = '1.0'
+__contact__ = 'https://t.me/tezmen'
